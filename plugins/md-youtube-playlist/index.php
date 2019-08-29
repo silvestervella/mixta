59ab4df681b4859458a320250fd7134dda1d2837
Silvester Vella <silvestervella@Silvesters-iMac.local>
2019-08-29 18:37:19 +0200

    gfxgn

---
+++ plugins/md-youtube-playlist/index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: MixtaDrama Theme - YouTube Playlist plugin
+Plugin URI: https://mixtadrama.com/
+Description: Play YouTube playlist
+Author: ..
+Version: 1.0
+*/
+
+function mixtadrama_youtube_playlist() {
+    $embed = "<iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list=UUwMJH-A_xa-T8kAvUZHiMhg' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+    return $embed;
+}
+	
+add_shortcode( 'youtube_playlist', 'mixtadrama_youtube_playlist' );
